115414f33772c95fc6001df9907bfa007bcee996
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-04-29 15:52:14 +0200

    Update main.js (adding omments)
    Adding comments

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ setInterval(snowFall,0); //initialisation de l'interval
 function snowFall() { //initialisation de la fonction de la neige qui tombe
   const snowDrop = document.createElement("i"); //"importation" de l'élément i (flocon de neige)
   let sw = Math.random() + .7; //initialisation de la variable sp (Speed Weight)
-  snowDrop.classList.add("fas"); //initialisation de la variable fas
-  snowDrop.classList.add("fa-snowflake");
+  snowDrop.classList.add("fas"); //importation de fas
+  snowDrop.classList.add("fa-snowflake");//importation de la classe fa-snowflake
   snowDrop.style.fontSize = sw * 7 + "px";
   sw = (1/sw);
   snowDrop.style.animationDuration = sw * 5 + "s";
   snowDrop.style.opacity = sw + 0.3;
   snowDrop.style.left = Math.random() * window.innerWidth + "px";
   body.appendChild(snowDrop)
-  setTimeout(() => {
-    snowDrop.remove();
-  }, 6666);
+  setTimeout(() => { //mise en place de la durée d'appartion ..
+    snowDrop.remove(); //suppression de l'élément du DOM
+  }, 6666); //.. à 6666 millisecondes
 }
